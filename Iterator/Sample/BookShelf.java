b0936dd4bdba115aef28216938c72a25ae698bc9
Shota Yamahira <seak0503@gmail.com>
2015-10-14 07:16:26 +0900

    第1章 練習問題

--- Iterator/Sample/BookShelf.java
+++ Iterator/Sample/BookShelf.java
@@ -1,18 +1,18 @@
+import java.util.*;
+
 public class BookShelf implements Aggregate {
-    private Book[] books;
-    private int last = 0;
-    public BookShelf(int maxsize) {
-        this.books = new Book[maxsize];
+    private ArrayList<Book> books;
+    public BookShelf() {
+        this.books = new ArrayList<Book>();
     }
     public Book getBookAt(int index) {
-        return books[index];
+        return books.get(index);
     }
     public void appendBook(Book book) {
-        this.books[last] = book;
-        last++;
+        this.books.add(book);
     }
     public int getLength() {
-        return last;
+        return last.size();
     }
     public Iterator iterator() {
         return new BookShelfIterator(this);
