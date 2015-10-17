34ec270178875c0f8b12ed4199303210a71f6091
Shota Yamahira <seak0503@gmail.com>
2015-10-18 05:55:15 +0900

    第３章 TemplateMethod

---
+++ TemplateMethod/Sample/CharDisplay.java
@@ -0,0 +1,15 @@
+public class CharDisplay extends AbstractDisplay {
+    private char ch;
+    public CharDisplay(char ch) {
+        this.ch = ch;
+    }
+    public void open() {
+        System.out.println("<<");
+    }
+    public void print() {
+        System.out.println(ch);
+    }
+    public void close() {
+        System.out.println(">>");
+    }
+}
