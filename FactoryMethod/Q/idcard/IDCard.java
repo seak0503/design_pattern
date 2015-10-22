4a78ec037193d5e5a182be7126ea1cbb2363870a
Shota Yamahira <seak0503@gmail.com>
2015-10-23 07:29:15 +0900

    第４章 練習問題

--- FactoryMethod/Q/idcard/IDCard.java
+++ FactoryMethod/Q/idcard/IDCard.java
@@ -2,15 +2,22 @@ package idcard;
 import framework.*;
 
 public class IDCard extends Product {
+    private static int cache = 0;
+    private int serial;
     private String owner;
     IDCard(String owner) {
         System.out.println(owner + "のカードを作ります。");
+        this.serial = cache;
         this.owner = owner;
+        cache++;
     }
     public void use() {
         System.out.println(owner + "のカードを使います。");
     }
     public String getOwner() {
         return owner;
+    }
+    public int getSerial() {
+        return serial;
     }
 }
--- FactoryMethod/Q/idcard/IDCardFactory.java
+++ FactoryMethod/Q/idcard/IDCardFactory.java
@@ -3,14 +3,14 @@ import framework.*;
 import java.util.*;
 
 public class IDCardFactory extends Factory {
-    private List<String> owners = new ArrayList<>();
+    private Map<Integer, String> owners = new HashMap<Integer, String>();
     protected Product createProduct(String owner) {
         return new IDCard(owner);
     }
     protected void registerProduct(Product product) {
-        owners.add(((IDCard)product).getOwner());
+        owners.put(((IDCard)product).getSerial(), ((IDCard)product).getOwner());
     }
-    public List getOwners() {
+    public Map getOwners() {
         return owners;
     }
 }
