10792e5cb8f6b7259ab078726164b0f5458fecf5
Shota Yamahira <seak0503@gmail.com>
2015-10-24 08:43:58 +0900

    5-1 Mainクラスをマルチスレッドにして、TicketMakerをマルチスレッド非対応にした時の実験

--- Singleton/Q1/Main.java
+++ Singleton/Q1/Main.java
@@ -1,9 +1,20 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Start.");
+public class Main extends Thread {
+    TicketMaker instance;
+    public Main(TicketMaker instance) {
+        this.instance = instance;
+    }
+    @Override
+    public void run() {
         for (int i = 0; i < 10; i++) {
-            System.out.println(i + ":" + TicketMaker.getInstance().getNextTicketNumber());
+            System.out.println(i + ":" + instance.getNextTicketNumber());
         }
-        System.out.println("End.");
+    }
+    public static void main(String[] args) {
+        TicketMaker instance = TicketMaker.getInstance();
+        new Main(instance).start();
+        new Main(instance).start();
+        new Main(instance).start();
+        new Main(instance).start();
+        new Main(instance).start();
     }
 }
