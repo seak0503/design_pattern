0f9289a5626fa1ff4df0d1c8f0d1a2bd02b248e3
Shota Yamahira <seak0503@gmail.com>
2015-10-24 08:10:59 +0900

    5-1 回答

--- Singleton/Q1/Main.java
+++ Singleton/Q1/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Start.");
-        int obj1 = TicketMaker.getNextTicketNumber();
-        int obj2 = TicketMaker.getNextTicketNumber();
-        System.out.println("obj1のチケット番号は " + obj1 + " です。");
-        System.out.println("obj2のチケット番号は " + obj2 + " です。");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + ":" + TicketMaker.getInstance().getNextTicketNumber());
+        }
+        System.out.println("End.");
     }
 }
--- Singleton/Q1/TicketMaker.java
+++ Singleton/Q1/TicketMaker.java
@@ -1,10 +1,12 @@
 public class TicketMaker {
-    private static int ticket = 1000;
-    private static TicketMaker ticketMaker = new TicketMaker();
+    private int ticket = 1000;
+    private static TicketMaker singleton = new TicketMaker();
     private TicketMaker() {
-        System.out.println("チケット番号生成を開始します。");
     }
-    public static int getNextTicketNumber() {
+    public static TicketMaker getInstance() {
+        return singleton;
+    }
+    public int getNextTicketNumber() {
         return ticket++;
     }
 }
