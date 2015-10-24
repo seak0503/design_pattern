face32205e1235554801160c059fee3cb8d9510f
Shota Yamahira <seak0503@gmail.com>
2015-10-25 08:14:22 +0900

    5-2 解答

--- Singleton/Q2/Main.java
+++ Singleton/Q2/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Start.");
-        for (int i = 0; 3 > i; i++) {
-            Triple obj = Triple.getInstance(i);
-            System.out.println(i + "番目のインスタンスを取得しました");
+        for (int i = 0; i < 9; i++) {
+            Triple triple = Triple.getInstance(i % 3);
+            System.out.println(i + ":" + triple);
         }
         System.out.println("End.");
     }
