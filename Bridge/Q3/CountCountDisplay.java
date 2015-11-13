237242a6fdc965d9df547b84510777ed05b7f13a
shyamahira <shyamahira@bbtower.co.jp>
2015-11-13 09:33:16 +0900

    問題9-3 完了

--- Bridge/Q3/CountCountDisplay.java
+++ Bridge/Q3/CountCountDisplay.java
@@ -2,4 +2,9 @@ public class CountCountDisplay extends CountDisplay {
     public CountCountDisplay(DisplayImpl impl) {
         super(impl);
     }
+    public void multiMultiDisplay(int times) {
+        for (int i = 0; i < times; i++) {
+            multiDisplay(i);
+        }
+    }
 }
--- Bridge/Q3/Main.java
+++ Bridge/Q3/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         Display d1 = new Display(new DecoDisplayImpl('<', '*', '>'));
+        CountCountDisplay d2 = new CountCountDisplay(new DecoDisplayImpl('<', '*', '>'));
         d1.display();
+        d2.multiMultiDisplay(5);
     }
 }
