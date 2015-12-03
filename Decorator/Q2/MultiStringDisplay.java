0b5693cff729143bf6261ad3ca4a588062559e1a
Shota Yamahira <seak0503@gmail.com>
2015-12-03 22:39:44 +0900

    解答12-2
    自分の考えたアルゴリズムと解答で差はないが、書き方が
    少し違っていた。
    
    ただし、解答の場合だと、半角文字列と全角文字列の問題で
    上下フレームの長さと、文字列の長さに差が出て、綺麗に出力されない

--- Decorator/Q2/MultiStringDisplay.java
+++ Decorator/Q2/MultiStringDisplay.java
@@ -1,39 +1,37 @@
 import java.util.*;
 
 public class MultiStringDisplay extends Display {
-    private ArrayList<String> strings;
-    private int maxlength;
-    public MultiStringDisplay() {
-        this.strings = new ArrayList<>();
-        this.maxlength = 0;
-    }
+    private ArrayList<String> strings = new ArrayList<>();
+    private int maxlength = 0;
     public void add(String string) {
         strings.add(string);
-        checkLength(string.length());
+        updateColumn(string);
     }
     public int getColumns() {
-        return maxlength * 2;
+        return maxlength;
     }
     public int getRows() {
         return strings.size();
     }
     public String getRowText(int row) {
-        return getString(strings.get(row));
+        return strings.get(row);
     }
-    private void checkLength(int length) {
-        if (maxlength < length) {
-            maxlength = length;
+    private void updateColumn(String string) {
+        if (string.getBytes().length > maxlength) {
+            maxlength = string.getBytes().length;
         }
-    }
-    private String getString(String string) {
-        int l = string.length();
-        if (maxlength > l) {
-            StringBuffer buf = new StringBuffer();
-            for (int i = 0; i < (maxlength - l); i++) {
-                buf.append("  ");
+        for (int row = 0; row < strings.size(); row++) {
+            int fills = maxlength - (strings.get(row)).getBytes().length;
+            if (fills > 0) {
+                strings.set(row, strings.get(row) + spaces(fills));
             }
-            string = string + buf;
         }
-        return string;
+    }
+    private String spaces(int count) {
+        StringBuffer buf = new StringBuffer();
+        for (int i = 0; i < count; i++) {
+            buf.append(' ');
+        }
+        return buf.toString();
     }
 }
