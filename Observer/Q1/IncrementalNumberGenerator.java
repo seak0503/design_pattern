06b4b50c6732a664831a78c03da693b895ee5ae7
Shota Yamahira <seak0503@gmail.com>
2016-01-26 07:16:09 +0900

    解答 17-1

--- Observer/Q1/IncrementalNumberGenerator.java
+++ Observer/Q1/IncrementalNumberGenerator.java
@@ -1,10 +1,9 @@
 public class IncrementalNumberGenerator extends NumberGenerator {
     private int number;
-    private int starting_number;
     private int terminate_number;
     private int incrementation_minute;
     public IncrementalNumberGenerator(int starting_number, int terminate_number, int incrementation_minute) {
-        this.starting_number = starting_number;
+        this.number = starting_number;
         this.terminate_number = terminate_number;
         this.incrementation_minute = incrementation_minute;
     }
@@ -12,9 +11,9 @@ public class IncrementalNumberGenerator extends NumberGenerator {
         return number;
     }
     public void execute() {
-        for (int i = starting_number; i < terminate_number; i += incrementation_minute) {
-            number = i;
+        while (number < terminate_number) {
             notifyObservers();
+            number += incrementation_minute;
         }
     }
 }
