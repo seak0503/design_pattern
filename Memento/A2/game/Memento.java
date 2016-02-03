be35c8894d2c62be9c80beda330ea89637d6acbb
Shota Yamahira <seak0503@gmail.com>
2016-02-04 07:14:17 +0900

    解答18-4

---
+++ Memento/A2/game/Memento.java
@@ -0,0 +1,21 @@
+package game;
+import java.util.*;
+import java.io.*;
+
+public class Memento implements Serializable {
+    int money;
+    ArrayList<String> fruits;
+    public int getMoney() {
+        return money;
+    }
+    Memento(int money) {
+        this.money = money;
+        this.fruits = new ArrayList<>();
+    }
+    void addFruit(String fruit) {
+        fruits.add(fruit);
+    }
+    List<String> getFruits() {
+        return (List)fruits.clone();
+    }
+}
