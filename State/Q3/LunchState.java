e5f66ded5d803eb9dcdf9522ed7145fb457b0ef1
Shota Yamahira <seak0503@gmail.com>
2016-02-07 07:41:27 +0900

    解答19-3

--- State/Q3/LunchState.java
+++ State/Q3/LunchState.java
@@ -6,12 +6,10 @@ public class LunchState implements State {
         return singleton;
     }
     public void doClock(Context context, int hour) {
-        if (9 <= hour && hour < 12)  {
-            context.changeState(DayState.getInstance());
-        } else if (13 <= hour && hour < 17) {
-            context.changeState(DayState.getInstance());
-        } else if (hour < 9 || 17 <= hour) {
+        if (hour < 9 || 17 <= hour) {
             context.changeState(NightState.getInstance());
+        } else if (9 <= hour && hour < 12 || 13 <= hour && hour < 17) {
+            context.changeState(DayState.getInstance());
         }
     }
     public void doUse(Context context) {
