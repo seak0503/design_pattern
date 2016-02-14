7185f4452c849fa7bf99b5ccf0b0f3e832a806e8
Shota Yamahira <seak0503@gmail.com>
2016-02-15 07:42:47 +0900

    解答20-1

--- Flyweight/Q1/BigString.java
+++ Flyweight/Q1/BigString.java
@@ -1,20 +1,26 @@
 public class BigString {
     private BigChar[] bigchars;
     public BigString(String string) {
+        initShared(string);
+    }
+    public BigString(String string, boolean shared) {
+        if (shared) {
+            initShared(string);
+        } else {
+            initUnshared(string);
+        }
+    }
+    private void initShared(String string) {
         bigchars = new BigChar[string.length()];
         BigCharFactory factory = BigCharFactory.getInstance();
         for (int i = 0; i < bigchars.length; i++) {
             bigchars[i] = factory.getBigChar(string.charAt(i));
         }
     }
-    public BigString(String string, boolean shared) {
-        if (shared) {
-            bigchars = new BigChar[string.length()];
-            for (int i = 0; i < bigchars.length; i++) {
-                bigchars[i] = new BigChar(string.charAt(i));
-            }
-        } else {
-            new BigString(string);
+    private void initUnshared(String string) {
+        bigchars = new BigChar[string.length()];
+        for (int i = 0; i < bigchars.length; i++) {
+            bigchars[i] = new BigChar(string.charAt(i));
         }
     }
     public void print() {
--- Flyweight/Q1/Main.java
+++ Flyweight/Q1/Main.java
@@ -5,7 +5,10 @@ public class Main {
             System.out.println("Example: java Main 1212123");
             System.exit(0);
         }
-        BigString bs = new BigString(args[0], true);
+        BigString bs;
+        bs = new BigString(args[0], false);
+        bs.print();
+        bs = new BigString(args[0], true);
         bs.print();
     }
 }
